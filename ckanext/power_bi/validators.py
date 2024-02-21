e44c59f4f76b8f3367b7793908d2761e94426e53
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-02-21 17:30:22 +0000

    feat(dev): finalize front-end;
    - Finalized front-end for Power BI report views.
    - Implemented i18n.

--- ckanext/power_bi/validators.py
+++ ckanext/power_bi/validators.py
@@ -3,7 +3,7 @@ from six import text_type
 
 from ckan.plugins.toolkit import _, Invalid
 
-REPORTD_ID_PATTERN = compile(
+REPORT_ID_PATTERN = compile(
     r'^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$')
 
 
@@ -12,6 +12,6 @@ def power_bi_report_id(value):
         raise Invalid(_("Power BI Report ID required."))
     if not isinstance(value, text_type):
         raise Invalid(_("Power BI Report ID must be a string."))
-    if not REPORTD_ID_PATTERN.match(value):
+    if not REPORT_ID_PATTERN.match(value):
         raise Invalid(_("Invalid value for a Power BI Report ID."))
     return value
