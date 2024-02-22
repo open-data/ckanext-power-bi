e26a4f7b622d5d27a7ddeaca8333c7dddef9121d
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-02-22 21:47:52 +0000

    fix(js): await full load;
    - Await full load before initializing ckan JS module.

--- ckanext/power_bi/assets/power_bi_view.js
+++ ckanext/power_bi/assets/power_bi_view.js
@@ -1,16 +1,26 @@
-this.ckan.module('power-bi-embed', function($){
-  return {
-    /* options object can be extended using data-module-* attributes */
-		options : {
-      config: {},
-		},
-    initialize: function (){
-      let reportContainer = $('#power-bi-container');
+window.addEventListener('load', function(){
 
-      if( reportContainer.length > 0 ){
-        powerbi.embed(reportContainer[0], this.options.config);
-      }
+  $(document).ready(function() {
 
-    }
-  };
+    this.ckan.module('power-bi-embed', function($){
+
+      return {
+        /* options object can be extended using data-module-* attributes */
+        options : {
+          config: {},
+        },
+        initialize: function (){
+          let reportContainer = $('#power-bi-container');
+
+          if( reportContainer.length > 0 ){
+            powerbi.embed(reportContainer[0], this.options.config);
+          }
+
+        }
+      };
+
+    });
+
+  });
+
 });
